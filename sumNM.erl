a0b9cecf7dad88f038336853121892e4312a6901
Roy Esibe <bichiroy50@gmail.com>
2019-01-10 06:31:02 +0100

    Roy|Write excercise Question

--- sumNM.erl
+++ sumNM.erl
@@ -1,3 +1,6 @@
+% Write a fubtion sum/2 which, given two integers N and M, where
+% N=<M,will return the sum of the interval between N and M. if N>M,
+%you want your process to terminate abnormally.
 -module (sumNM).
 -export ([sum/2]).
 
