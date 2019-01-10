7e052b609dcd378b58f257f977597cf6974c9859
Roy Esibe <bichiroy50@gmail.com>
2019-01-10 17:12:34 +0100

    Add exception and put recursive base case

--- sumNM.erl
+++ sumNM.erl
@@ -1,13 +1,9 @@
 % Write a fubtion sum/2 which, given two integers N and M, where
 % N=<M,will return the sum of the interval between N and M. if N>M,
-%you want your process to terminate abnormally.
+%you want your process to terminate abnormally
 -module (sumNM).
 -export ([sum/2]).
 
-sum( N, M) ->
-	if
-		N =:= M -> N;
-
-		N < M -> N + sum(N+1, M)
-
-	end.
+sum(N,N) -> N; %base case 
+sum(N,M) when N < M -> N + sum(N+1,M);
+sum(_, _) -> erlang:error("Invalid inputs"). %error message added
